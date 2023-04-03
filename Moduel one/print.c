b8469b9876158f150d00a27ff7c2ebc95aad27b2
sakib-al-hasan <md.sakib.al.hasan.programmer@gmail.com>
2023-04-03 08:15:16 +0600

    first commit

---
+++ Moduel one/print.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main () {
+    int tk;
+    scanf("%d",&tk);
+    printf("%d",tk);
+    return 0;
+}
--- setup/hello.c
+++ setup/hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main() 
-{   printf("hello man");
+{
+    printf("man is mortal");
     return 0;
 }
